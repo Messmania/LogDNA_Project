ff648879cee56723a68a775fe1805b38a8f6c528
Messmania <monikasahai03@gmail.com>
2019-11-27 01:30:24 -0500

    Modifications to server

--- blog.js
+++ blog.js
@@ -1,33 +1,29 @@
 
 
 const submitMessage = () => {
-    console.log("Clicked!")
     let input = document.getElementById('msg');
     console.log(input.value);
-    let p = fetch("https://localhost:3000", {
+    let p = fetch("http://localhost:3000", {
         method: 'POST',
         body: JSON.stringify(input.value)
     });
     p.then(
         (success) => {
-            // console.log("Success!:", success);
             input.value="";
+
         },
         (err) => {
-            // console.log("Error!", err);
             let errorComp= document.getElementById('errorText');
             errorComp.innerHTML = err;
             document.getElementById('error').style.visibility = 'visible';
         });
 }
 
-const closeNotif = (event) => {
-    // console.log("CLosed!");
+const closeNotif = () => {
     document.getElementById('error').style.visibility='hidden';
 }
 
 const checkMessageLength = (event) =>{
-    console.log(event.target.value);
     if(event.target.value.length){
         document.getElementsByClassName('submitButton')[0].disabled = false;
     }else{
--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
 
 const http = require('http');
 
-const requestHandler = (request, response)=>{
+const requestHandler = (request, response) => {
     response.setHeader('Access-Control-Allow-Methods', 'POST');
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.setHeader('Access-Control-Allow-Headers', '*');
-    console.log("Hello", request.url);
-    let body = '';
-    request.on('data', stream => {
-        body += stream.toString(); // convert Buffer to string
-    });
-    request.on('end', () => {
-        console.log('Testing:', body);
-        response.end('ok');
-    });
-    // response.end("Nodejs server");
+    console.log(request.method, "request arrived");
+    if (request.method == 'POST') {
+        let body = '';
+        request.on('data', stream => {
+            body += stream.toString(); // convert Buffer to string
+        });
+        request.on('end', () => {
+            response.end('OK');
+        });
+    }
 }
 
 const myServer = http.createServer(requestHandler);
-myServer.listen(3000, (err)=>{
+myServer.listen(3000, (err) => {
     console.log("Listening");
 })
