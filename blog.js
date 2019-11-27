c16c50d268127f57aa24b77f6477fdb838472311
Messmania <monikasahai03@gmail.com>
2019-11-27 01:58:27 -0500

    Code cleanup

--- blog.js
+++ blog.js
@@ -1,32 +1,32 @@
 
-
+/** 
+ * Button click handler
+ * Makes a POST request to the server
+ */
 const submitMessage = () => {
     let input = document.getElementById('msg');
-    console.log(input.value);
-    let p = fetch("http://localhost:3000", {
-        method: 'POST',
-        body: JSON.stringify(input.value)
-    });
-    p.then(
-        (success) => {
-            input.value="";
-
-        },
-        (err) => {
-            let errorComp= document.getElementById('errorText');
-            errorComp.innerHTML = err;
-            document.getElementById('error').style.visibility = 'visible';
+    if (input.value.length > 0) {
+        let p = fetch("http://localhost:3000", {
+            method: 'POST',
+            body: JSON.stringify(input.value)
         });
-}
-
-const closeNotif = () => {
-    document.getElementById('error').style.visibility='hidden';
-}
-
-const checkMessageLength = (event) =>{
-    if(event.target.value.length){
-        document.getElementsByClassName('submitButton')[0].disabled = false;
-    }else{
-        document.getElementsByClassName('submitButton')[0].disabled = true;
+        p.then(
+            (success) => {
+                input.value = "";
+            },
+            (err) => {
+                let errorComp = document.getElementById('errorText');
+                errorComp.innerHTML = err;
+                document.getElementById('error').style.visibility = 'visible';
+            });
     }
 }
+
+/**
+ * Closes the notification bar
+ */
+const closeNotif = () => {
+    document.getElementById('error').style.visibility = 'hidden';
+}
+
+
